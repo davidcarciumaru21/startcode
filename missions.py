12bc54ce9a5886dfd825adf7412bef93e80eb1dc
David <davidcarciumaru21@gmail.com>
2025-02-16 11:05:04 +0200

    added run 04

--- missions.py
+++ missions.py
@@ -15,7 +15,7 @@ from setup import Robot
 # ************ GLOBAL VALUES ************
 
 # Lista de run-uri pe care robotul le poate face
-runs = [run01, run02, run03]
+runs = [run01, run02, run03, run04]
 
 nemo = Robot(49.5, 113)
 nemo.d.settings(2000, 2000, 2000, 2000)
@@ -34,39 +34,56 @@ nemo02.d.settings(2000, 2000, 2000, 2000)
 nemo03 = Robot(49.5, 115)
 nemo03.d.settings(2000, 2000, 2000, 2000)
 
+nemo04 = Robot(49.5, 115)
+nemo04.d.settings(2000, 2000, 2000, 2000)
+
 # ************ RUNS ************
 
-# colectare stânga
-def run01():
+def run01(): #colectare stanga
     wait(200)
 
-# recif, rechin, scubi
-def run02():
+
+def run02(): #recif, rechin, scubi
     wait(200)
-    nemo02.d.straight(650)  
-    nemo02.d.turn(-90)  
-    nemo02.d.straight(90)  # Ajunge la recif
+    nemo02.d.straight(660)
+    wait(100)
+    nemo02.bratDr.run_angle(500, 250) #pozitie colectare scubi
+    wait(100)
+    nemo02.d.turn(-90)
+    wait(100)
+    nemo02.bratSt.run_angle(-200, 180) #brat jos sub recif
+    
+    
+    nemo02.d.straight(60) #la recif
+    nemo02.bratSt.run_angle(100, 170) #ridicare recif
+    #nemo02.bratSt.run_angle(-100, 30)
+    nemo02.bratDr.run_angle(-150, 80) #luat scubi
+
     nemo02.d.straight(-100)
-    nemo02.d.turn(40)
-    nemo02.d.straight(290)  # Spre rechin
-    nemo02.d.straight(-360)
-    nemo02.d.turn(40)
-    nemo02.d.straight(300)  # Misiunea verde
-    nemo02.d.straight(-100)
+    nemo02.d.turn(95)
+    nemo02.d.straight(120)
+    nemo02.bratDr.run_angle(150, 80) #dus scubi
+    ''''nemo02.d.straight(-100)
     nemo02.d.turn(20)
     nemo02.d.straight(-700)
-    nemo02.stopRobot()
+    nemo02.stopRobot()'''
 
-# colectare stânga
-def run03():
+def run03(): #catarg, dus rechin, nava, colectare trident si un crevete
     wait(200)
-    nemo03.d.straight(550)  
-    nemo03.d.turn(88)  
-    nemo03.d.straight(150)  # Ridicare catarg
+    nemo03.d.straight(550)
+    nemo03.d.turn(88)
+    nemo03.d.straight(150) #ridicare catarg
     nemo03.d.straight(-50)
     nemo03.d.turn(70)
     nemo03.d.straight(420)
     nemo03.d.turn(-130)
-    nemo03.d.straight(400)  # Spre navă
-    # Trident și crevete
+    nemo03.d.straight(400) #spre nava
+    #trident si crevete
     nemo03.d.straight(-600)
+
+def run04():#barca, anglerfish, verde rotund, colectare dreapta, baza dreapta
+    wait(200)
+    nemo04.d.straight(200)
+    nemo04.d.turn(90)
+    nemo04.d.straight(800)
+    nemo04.d.turn(45)
