63e5d3dc03e603fa58aaf9f3a584e2db0dd771fb
David <davidcarciumaru21@gmail.com>
2025-02-18 20:11:41 +0200

    modified runs

--- missions.py
+++ missions.py
@@ -53,9 +53,18 @@ def run01(): #colectare stanga
 def run02(): #recif, rechin, scubi
     wait(200)
     nemo02.d.turn(5)
-    nemo02.d.straight(670)
+    nemo02.d.straight(710)
+    #nemo02.d.straight(60)
+    #nemo02.d.turn(5)
+    nemo02.bratDr.run_angle(800, 600)
+    nemo02.bratDr.run_angle(-800, 430)
+    #nemo02.d.turn(-5)
+    nemo02.d.straight(-30)
+    nemo02.bratDr.run_angle(800, 40)
+    
+
     wait(100)
-    nemo02.bratDr.run_angle(800, 260) #pozitie colectare scubi
+    #nemo02.bratDr.run_angle(800, 210) #pozitie colectare scubi
     wait(100)
     nemo02.d.turn(-95)
     wait(100)
@@ -63,32 +72,45 @@ def run02(): #recif, rechin, scubi
     
     
     nemo02.d.straight(105) #la recif
-    nemo02.bratDr.run_angle(-150, 110) #luat scubi
-    #nemo02.d.straight(-5)
-    nemo02.d.turn(7)
-    nemo02.bratSt.run_angle(-400, 570) #ridicare recif
+    nemo02.bratDr.run_angle(-150, 170) #luat scubi
     wait(100)
-    nemo02.bratSt.run_angle(200, 120)
+    nemo02.d.turn(7)
+    nemo02.bratSt.run_angle(-350, 610) #ridicare recif
+    #wait(100)
+    nemo02.bratSt.run_angle(600, 170)
     
 
-    nemo02.d.straight(-90)
-    nemo02.d.turn(100)
-    nemo02.d.straight(70)
+    nemo02.d.straight(-100)
+    nemo02.d.turn(97)
+    nemo02.d.straight(50)
     
-    #nemo02.d.turn(10)
-    #nemo02.d.straight(20)
-    nemo02.bratDr.run_angle(200, 140) #dus scubi
-    nemo02.d.straight(-40)
+
+    nemo02.bratDr.run_angle(200, 220) #dus scubi
+    nemo02.d.straight(-70)
     nemo02.d.turn(-10)
+    nemo02.bratSt.run_angle(600, 470)
+    nemo02.bratSt.run_angle(-1000, 600)
+    #nemo02.d.straight(-830)
+    nemo02.d.reset()
+    while nemo02.d.distance() > -700:
+        nemo02.d.drive(-300, 10)
+    nemo02.d.straight(-5)
+    nemo02.d.stop()
+    
+    '''
+    nemo02.d.turn(-80)
     nemo02.d.straight(30)
+    nemo02.d.turn(80)
+    nemo02.d.straight(20)
+    
     nemo02.bratSt.run_angle(500, 500)
     nemo02.bratDr.run_angle(500, 300)
     wait(100)
     nemo02.bratSt.run_angle(-500, 500)
-    nemo02.bratDr.run_angle(-500, 80)
-    nemo02.d.straight(-500)
+    nemo02.bratDr.run_angle(-500, 300)
+    nemo02.d.straight(-700)
     
-    '''
+    
     nemo02.d.straight(-600)
     
     nemo02.d.straight(-100)
@@ -98,16 +120,10 @@ def run02(): #recif, rechin, scubi
 
 def run03(): #catarg, dus rechin, nava, colectare trident si un crevete
     wait(200)
-    nemo03.d.straight(550)
-    nemo03.d.turn(88)
-    nemo03.d.straight(150) #ridicare catarg
-    nemo03.d.straight(-50)
-    nemo03.d.turn(70)
-    nemo03.d.straight(420)
-    nemo03.d.turn(-130)
-    nemo03.d.straight(400) #spre nava
-    #trident si crevete
-    nemo03.d.straight(-600)
+    nemo03.d.straight(200)
+    nemo03.d.turn(90)
+    nemo03.d.straight(200)
+    nemo03.d.turn(-45)
 
 def run04():#barca, anglerfish, verde rotund, colectare dreapta, baza dreapta
     wait(200)
@@ -123,5 +139,5 @@ run02Obj = Mission(run02, "recif, rechin", Color.YELLOW)
 run03Obj = Mission(run03, "recif", Color.WHITE)
 run04Obj = Mission(run04, "barca", Color.BLUE)
 
-blueRuns = [run01Obj, run02Obj]
-redRuns = [run03Obj, run04Obj]
+blueRuns = []
+redRuns = [run01Obj, run02Obj, run03Obj, run04Obj]
