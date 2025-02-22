a784c95f18354abfc9a41bf19e184614f3bd2ba0
David <davidcarciumaru21@gmail.com>
2025-02-22 11:13:23 +0200

    bug fix

--- missions.py
+++ missions.py
@@ -8,9 +8,9 @@ from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
-from mission import Mission
 
-from robot import Robot 
+from robot import Robot
+from run import Run 
 
 # ************ ROBOT OBJECTS ************
 
